fcc554b13c4e71b303df09358dbad40d7f02d986
rupert-ong <rupert.ong@gmail.com>
2018-02-02 21:05:13 -0500

    Add error handling

--- app.js
+++ app.js
@@ -5,4 +5,20 @@ const ninjaRoutes = require('./api/routes/ninjas');
 
 app.use('/api/ninjas', ninjaRoutes);
 
+// 404 Error
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
+// Error Handling
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    error: {
+      message: err.message
+    }
+  });
+});
+
 module.exports = app;

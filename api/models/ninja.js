e26765adbe7d9ed04d65f5245625053d00f3c197
rupert-ong <rupert.ong@gmail.com>
2018-02-02 21:36:55 -0500

    Add mongoose dependency. Create Ninja model and schema.

---
+++ api/models/ninja.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const NinjaSchema = new Schema({
+  name: {
+    type: String,
+    required: [true | 'Name field is required']
+  },
+  rank: {
+    type: String
+  },
+  available: {
+    type: Boolean,
+    default: false
+  }
+  // TODO: Add in geolocation definition
+});
+
+module.exports = mongoose.model('Ninja', NinjaSchema);
